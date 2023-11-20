9317c718a1200da842b9814e27f320981701e9ac
Your Name <sumitsingh89570@gmail.com>
2023-11-20 19:32:08 +0530

    header/navbar component created

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,12 @@ import Signup from "./pages/Signup";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
 import Home from "./pages/Home";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <BrowserRouter>
+    <Header/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<Signin />} />
