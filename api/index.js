2e59152f68acdf217faf5f6aea2352d60a1674f9
Your Name <sumitsingh89570@gmail.com>
2024-02-09 12:59:40 +0530

    changes before deploying

--- api/index.js
+++ api/index.js
@@ -5,34 +5,41 @@ import userRouter from "./routes/user.router.js"
 import authRouter from "./routes/auth.router.js"
 import listingRouter from "./routes/listing.router.js"
 import cookieParser from "cookie-parser";
+import path from 'path';
 dotenv.config();
 
-mongoose.connect(process.env.MONGO).then(()=>{
+mongoose.connect(process.env.MONGO).then(() => {
     console.log("Connected to MongoDB");
-}).catch((err)=>{
+}).catch((err) => {
     console.log(err);
 })
 
+const __dirname = path.resolve();
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-app.listen(4000,()=>{
+app.listen(4000, () => {
     console.log("Server is running on port 4000!")
 })
 
-app.use('/api/user',userRouter);
-app.use('/api/auth',authRouter);
-app.use('/api/listing',listingRouter);
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/listing', listingRouter);
 
+app.use(express.static(path.join(__dirname, '/client/dist')));
+app.get('*', (req, resp) => {
+    resp.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+
+});
 
 // middleware for error---
-app.use((err,req,resp,next)=>{
+app.use((err, req, resp, next) => {
     const statusCode = err.statusCode || 500;
     const message = err.message || "Internal Server Error";
 
     return resp.status(statusCode).json({
-        success:false,
+        success: false,
         statusCode,
         message,
     })      // key aur value same hone pr kewal key bhej skte h
