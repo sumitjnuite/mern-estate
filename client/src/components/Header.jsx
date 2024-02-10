37968ca403880887c4ac33191cd6893e5c8ee81c
Your Name <sumitsingh89570@gmail.com>
2024-02-10 14:22:07 +0530

    logo and favicon added

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { FaSearch } from "react-icons/fa"; // npm i react-icons
 import { useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
+import ApnaPlaceLogo from '../assets/ApnaPlaceLogo.png';
 
 const Header = () => {
   const { currentUser } = useSelector((state) => state.user);
@@ -29,9 +30,10 @@ const Header = () => {
     <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
         <Link to="/">
-          <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
-            <span className="text-slate-500">Sumit</span>
-            <span className="text-slate-700">Estate</span>
+          <h1 className="font-bold text-sm sm:text-xl flex flex-wrap items-center">
+            <img src={ApnaPlaceLogo} alt="logo" className="h-[40px] mx-2"/>
+            <span className="text-slate-500">Apna</span>
+            <span className="text-slate-700">Place</span>
           </h1>
         </Link>
         <form
